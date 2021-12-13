bacc88040c4965d63aa8ce5a0ef950a635e379c7
eshanajain <82397896+eshanajain@users.noreply.github.com>
2021-12-13 20:47:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,28 +12,24 @@ Webcam.attach ('#camera');
 function take_snapshot()
 {
     Webcam.snap(function(data_uri){
-        document.getElementById("results").innerHTML = '<img id="captured image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 };
 
 console.log('ml5 version: ', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/rZ6l8iTEC/model.json',modelloaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/rZ6l8iTEC/model.json',modelLoaded);
 
-function modelloaded() {
+function modelLoaded() {
     console.log('Model Loaded!');
 }
 
 function speak(){
     var synth = window.speechSynthesis;
     speak_data_1 = "The first prediction is" + prediction_1;
-    speak_data_2 = "The first prediction is" + prediction_2;
+    speak_data_2 = "The second prediction is" + prediction_2;
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2)
     synth.speak(utterThis);
 }
-
-function modelLoaded() {
-    console.log('Model Loaded!');
-};
 
 function check()
 {
@@ -43,7 +39,7 @@ function check()
 
 function gotResult(error, results) {
     if (error) {
-        console.error(eror);
+        console.error(error);
     }else{
         console.log(results);
         document.getElementById("result_guesture_name").innerHTML = results[0].label;
